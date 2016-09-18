619019cd9ccce8b59b8d66aeb9f019a8eb2303b1
André Caldegren <andweij@gmail.com>
2016-09-18 13:12:15 +0200

    Börjat med bootstrap

---
+++ bootystrap/index.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>NASA - Bootstrap</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+		<style type="text/css">
+			body
+			{
+				background-color: black;
+				margin-top: 15px;
+			}
+			.a
+			{
+				background-color: white;
+				margin-bottom: 15px;
+				height: 300px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12"><div class="a col-md-12">Jumbo</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-3"><div class="a col-md-12">Small</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-6"><div class="a col-md-12">Double</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-3"><div class="a col-md-12">Small</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-6"><div class="a col-md-12">Double</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-6"><div class="a col-md-12">Double</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-3"><div class="a col-md-12">Small</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-3"><div class="a col-md-12">Small</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-3"><div class="a col-md-12">Small</div></div>
+				<div class="col-sx-12 col-sm-6 col-md-3"><div class="a col-md-12">Small</div></div>
+			</div>
+		</div>
+	</body>
+</html>
