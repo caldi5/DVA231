a688ea47839b25e8899882cc8b0a8228049ccda8
André Caldegren <andweij@gmail.com>
2016-09-18 22:26:34 +0200

    login looks nicer

--- bootystrap/login.php
+++ bootystrap/login.php
@@ -16,10 +16,52 @@
 	}
 ?>
 
-<body>
-	<form method="post">
-		username<input type="text" name="username">
-		password<input type="password" name="password">
-		<input type="submit" name="login" value="Login">
-	</form>
-</body>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>NASA - Login</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+
+		<style type="text/css">
+			.form-signin
+			{
+				margin: 0 auto;
+				max-width: 330px;
+			}
+			.form-signin-heading
+			{
+				margin-bottom: 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+		<!--
+			<form class="form-signin" method="post" action="login.php">
+				<h2 class="form-signin-heading">Sign in</h2>
+				username<input type="text" name="username">
+				password<input type="password" name="password">
+				<input type="submit" name="login" value="Login">
+			</form>-->
+
+			<form class="form-signin" action="login.php" method="post">
+				<h2 class="form-signin-heading">Please sign in</h2>
+
+				<label for="username" class="sr-only">Email address</label>
+				<input name="username" type="text" class="form-control" placeholder="Email address" required="" autofocus="">
+
+				<label for="password" class="sr-only">Password</label>
+				<input name="password" type="password" class="form-control" placeholder="Password" required="">
+
+				<button class="btn btn-lg btn-primary btn-block" name="login" type="submit">Sign in</button>
+			</form>
+		</div>
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</body>
+</html>
