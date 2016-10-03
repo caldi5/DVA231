883480e4ffe5b9f48b68dc2ef58cd1b1619ef860
Enari <Spam@minit.nu>
2016-10-03 21:20:30 +0200

    Pretty error messages for dbconn

--- bootystrap/dbconn.php
+++ bootystrap/dbconn.php
@@ -10,7 +10,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    //die("Connection failed: " . $conn->connect_error);
+    $error = "Connection failed: " . $conn->connect_error;
     $conn->close();
 }
 
